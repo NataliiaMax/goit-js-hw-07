6b5942105b72f2efd5b6f7d2c1a38d741e5a6d4d
NataliiaMax <105719575+NataliiaMax@users.noreply.github.com>
2022-10-06 15:16:31 +0300

    update gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,15 +1,51 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-const divRef = document.querySelector('.gallery');
-divRef.addEventListener('click', event =>
-{
-    event.preventDefault();
-    if (event.target.nodeName !== "A") {
-      return;
-    }
-    
-    console.log(onClick.target.nodeName);
-})
-// const instance = basicLightbox.create(document.querySelector("#template"));
-// instance.show();
+const divRef = document.querySelector(".gallery");
+const largeImageRef = document.querySelector(".gallery__image");
+
+const arrayGallery = galleryItems.map((image) => {
+  return `<div class="gallery__item">
+    <a class="gallery__link" href="${image.original}">
+      <img
+        class="gallery__image"
+        src="${image.preview}"
+        data-source="${image.original}"
+        alt="${image.description}"
+      />
+    </a>
+  </div>`;
+});
+divRef.insertAdjacentHTML("afterbegin", arrayGallery);
+console.log(arrayGallery);
+
+divRef.addEventListener("click", (event) => {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  const instance = basicLightbox.create(`
+	<img src = "${event.target.dataset.source}" width="800" height="600">
+`);
+  instance.show();
+});
 console.log(galleryItems);
+
+document.addEventListener("keydown", (event) => {
+  console.log("Keydown: ", event);
+});
+// const instance = basicLightbox.create(
+//   `
+//     <div class="modal">
+//         <p>A custom modal that has been styled independently. It's not part of basicLightbox, but perfectly shows its flexibility.</p>
+//         <input placeholder="Type something">
+//         <a>Close</a>
+//     </div>
+// `,
+//   {
+//     onShow: (instance) => {
+//       instance.element().querySelector("a").onclick = instance.close;
+//     },
+//   }
+// );
+
+// instance.show();
